d0314b06352385139ddfb9ce9eb257c97e425ded
Panepo <whisper.ko@gmail.com>
2022-07-29 13:59:06 +0800

    update function
    update function

--- csharp/Common/TesseractOCR.cs
+++ csharp/Common/TesseractOCR.cs
@@ -1,9 +1,11 @@
 using System;
 using System.Collections.Generic;
 using System.Drawing;
+using System.IO;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using System.Windows.Forms;
 using Tesseract;
 
 namespace Kikuzuki
@@ -17,13 +19,66 @@ namespace Kikuzuki
             Engine = new TesseractEngine(dataPath, language);
         }
 
-        public string ProcessBMP(string imagePath)
+        public string ProcessFile(string imagePath)
         {
-            Pix Pix = PixConverter.ToPix(new Bitmap(imagePath));
-            using (var page = Engine.Process(Pix))
+            try
             {
-                string text = page.GetText();
-                return text;
+                using (Image Dummy = Image.FromFile(imagePath))
+                {
+                    Pix Pix = PixConverter.ToPix(new Bitmap(Dummy));
+                    using (var page = Engine.Process(Pix))
+                    {
+                        string text = page.GetText();
+                        return text;
+                    }
+                }
+            }
+            catch (FileNotFoundException e)
+            {
+                throw new FileNotFoundException(@"File not found.", e);
+            }
+        }
+
+        public string ProcessScreen()
+        {
+            Rectangle Bounds = Screen.GetBounds(Point.Empty);
+
+            using (Bitmap bitmap = new Bitmap(Bounds.Width, Bounds.Height))
+            {
+                using (Graphics graphic = Graphics.FromImage(bitmap))
+                {
+                    graphic.CopyFromScreen(Point.Empty, Point.Empty, Bounds.Size);
+                }
+
+                Pix Pix = PixConverter.ToPix(new Bitmap(bitmap));
+                using (var page = Engine.Process(Pix))
+                {
+                    string text = page.GetText();
+                    return text;
+                }
+            }
+        }
+
+        public string ProcessScreen(Point x, Point y, Size size, bool saveImage = false)
+        {
+            using (Bitmap bitmap = new Bitmap(size.Width, size.Height))
+            {
+                using (Graphics graphic = Graphics.FromImage(bitmap))
+                {
+                    graphic.CopyFromScreen(x, y, size);
+                }
+
+                if (saveImage)
+                {
+                    bitmap.Save("screenCapture.bmp");
+                }
+
+                Pix Pix = PixConverter.ToPix(new Bitmap(bitmap));
+                using (var page = Engine.Process(Pix))
+                {
+                    string text = page.GetText();
+                    return text;
+                }
             }
         }
     }
--- csharp/OCR/Program.cs
+++ csharp/OCR/Program.cs
@@ -3,6 +3,7 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using System.Drawing;
 
 namespace Kikuzuki
 {
@@ -11,7 +12,10 @@ namespace Kikuzuki
         static void Main()
         {
             TesseractOCR Ocr = new TesseractOCR(AppDomain.CurrentDomain.BaseDirectory, "eng");
-            Console.WriteLine(Ocr.ProcessBMP("1.bmp"));
+
+            Console.WriteLine("OCR Test");
+            Console.WriteLine(Ocr.ProcessScreen( new Point(0), new Point(0), new Size(300, 300 ), true ));
+            
             Console.ReadKey();
         }
     }
